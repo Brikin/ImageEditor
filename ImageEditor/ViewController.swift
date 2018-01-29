f6e5d66ce6da7d2b5d64bcb96b60212b3cef8cf2
Ruslan Brikin <ruslan.brikin@icloud.com>
2018-01-29 22:10:34 +0700

    implement invertColors

--- ImageEditor/ViewController.swift
+++ ImageEditor/ViewController.swift
@@ -154,17 +154,35 @@ extension ViewController: UICollectionViewDelegate {
     
 }
 
+enum ImageModificationType {
+    case mirror, invertColors, rotate, grayScale
+}
+
 extension UIImage {
     
     func convert(type: ImageModificationType) -> UIImage {
         switch type {
         case .invertColors:
-            return self.convertImageToBW()
+            return self.invertColors(cgResult: true)!
         case .mirror:
             return self.imageRotatedByDegrees(degrees: 0, flip: true)
         case .rotate:
             return self.imageRotatedByDegrees(degrees: 90, flip: false)
+        case .grayScale:
+            return self.convertImageToBW()
+        
         }
+    }
+    
+    func invertColors(cgResult: Bool) -> UIImage? {
+        let coreImage = UIKit.CIImage(image: self)
+        guard let filter = CIFilter(name: "CIColorInvert") else { return nil }
+        filter.setValue(coreImage, forKey: kCIInputImageKey)
+        guard let result = filter.value(forKey: kCIOutputImageKey) as? UIKit.CIImage else { return nil }
+        if cgResult { // I've found that UIImage's that are based on CIImages don't work with a lot of calls properly
+            return UIImage(cgImage: CIContext(options: nil).createCGImage(result, from: result.extent)!)
+        }
+        return UIImage(ciImage: result)
     }
     
     func imageRotatedByDegrees(degrees: CGFloat, flip: Bool) -> UIImage {
@@ -223,9 +241,7 @@ extension UIImage {
     }
 }
 
-enum ImageModificationType {
-    case mirror, invertColors, rotate
-}
+
 
 
 
